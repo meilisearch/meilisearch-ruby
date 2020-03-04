abbf1bc9fe7b26c5ede3fe04cea730221f238fb5
Clementine Urquizar <clementine@meilisearch.com>
2020-03-04 18:57:50 +0100

    Upd release script

--- scripts/check-release.sh
+++ scripts/check-release.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-# Checking if current branch is master
-current_branch=${GITHUB_REF}
-if [ "$current_branch" != 'master' ]; then
-  echo "Error: the current branch must be master."
-  echo "Current branch: $current_branch"
-  exit 1
-fi
-
 # Checking if current tag matches the package version
-current_tag=$(git describe --tag | tr -d 'v')
+current_tag=$(echo $GITHUB_REF | tr -d 'refs/tags/v')
 file_tag=$(grep 'VERSION = ' lib/meilisearch/version.rb | cut -d "=" -f 2- | tr -d ' ' | tr -d \')
 if [ "$current_tag" != "$file_tag" ]; then
   echo "Error: the current tag does not match the version in package file(s)."
