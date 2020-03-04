da382f68c679d9f3f5c0467e152f935fe2cb068b
Clementine Urquizar <clementine@meilisearch.com>
2020-03-04 18:31:18 +0100

    Add script to check release

---
+++ scripts/check-release.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Checking if current branch is master
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != 'master' ]; then
+  echo "Error: the current branch must be master."
+  echo "Current branch: $current_branch"
+  exit 1
+fi
+
+# Checking if current tag matches the package version
+current_tag=$(git describe --tag | tr -d 'v')
+file_tag=$(grep 'VERSION = ' lib/meilisearch/version.rb | cut -d "=" -f 2- | tr -d ' ' | tr -d \')
+if [ "$current_tag" != "$file_tag" ]; then
+  echo "Error: the current tag does not match the version in package file(s)."
+  echo "$current_tag vs $file_tag"
+  exit 1
+fi
+
+echo 'OK'
+exit 0
