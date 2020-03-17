fcaf4e9d9281ad7c5fc15381771db4a755575352
Clémentine Urquizar <clementine@meilisearch.com>
2020-03-17 15:43:49 +0100

    Update README.md
    Co-Authored-By: Thomas Payet <thomas@meilisearch.com>

--- spec/meilisearch/client/health_spec.rb
+++ spec/meilisearch/client/health_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe MeiliSearch::Client::Health do
   let(:client)       { MeiliSearch::Client.new($URL, $MASTER_KEY) }
-  let(:wrong_client) { MeiliSearch::Client.new('nope') }
+  let(:wrong_client) { MeiliSearch::Client.new('bad_url') }
 
   it 'is healthy when the url is valid' do
     expect(client.healthy?).to be true
