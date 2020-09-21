70c07b65d0bcf587a6175e2c4272e6cc3af0039d
Anton Ilin <bronislav@users.noreply.github.com>
2020-09-22 00:16:35 +0300

    feat: add ability to set bigger timeout and max_retries option (#86)
    * feat: add ability to set bigger timeout and max_retries option
    
    In version 0.17.1 httparty introduces support for max_retries
    option. Since this gem already depends on httparty >= 0.17.1,
    I think it is safe to add support for this feature without any other
    conditions. In order to keep backward compatibility, default value
    for `max_retries` options is set to 1, which means that retries are
    disabled by default.
    
    Added ability to configure request timeout. Default version for this
    options stays same and equals 1 second. User can set bigger or smaller
    timeout.
    
    * Add docs and tests (#1)
    
    Co-authored-by: Clémentine Urquizar <clementine@meilisearch.com>

--- spec/meilisearch_spec.rb
+++ spec/meilisearch_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe MeiliSearch do
       client.indexes
     end.to raise_error(MeiliSearch::CommunicationError)
   end
+
+  it 'allows to set a custom timeout and max_retries' do
+    client = MeiliSearch::Client.new($URL, $MASTER_KEY, timeout: 0, max_retries: 2)
+    expect do
+      client.indexes
+    end.to raise_error(Timeout::Error)
+  end
 end
