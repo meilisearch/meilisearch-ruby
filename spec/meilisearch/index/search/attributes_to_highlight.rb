850e774d18625afc289946b8f7ad6ecfac3534ed
CaroFG <carolina.ferreira131@gmail.com>
2020-11-12 17:03:59 +0100

    Add a particular attribute to be highlighted

--- spec/meilisearch/index/search/attributes_to_highlight.rb
+++ spec/meilisearch/index/search/attributes_to_highlight.rb
@@ -35,11 +35,12 @@ RSpec.describe 'MeiliSearch::Index - Search with highlight' do
   end
 
   it 'does a custom search with highlight' do
-    response = @index.search('the', attributesToHighlight: ['*'])
+    response = @index.search('the', attributesToHighlight: ['title'])
     expect(response).to be_a(Hash)
     expect(response.keys).to contain_exactly(*default_search_response_keys)
     expect(response['hits'].count).to eq(3)
     expect(response['hits'].first).to have_key('_formatted')
+    expect(response['hits'].first['_formatted']['title']).to eq('<em>The</em> Hobbit')
   end
 
   it 'does a custom placeholder search with attributes to highlight' do
