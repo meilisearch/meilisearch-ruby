ae8bbc8053accd3a24cb17ecb884c95ead2d30ed
CaroFG <carolina.ferreira131@gmail.com>
2020-11-12 16:44:55 +0100

    Update documents and test with a higher croplength

--- spec/meilisearch/index/search/attributes_to_crop_spec.rb
+++ spec/meilisearch/index/search/attributes_to_crop_spec.rb
@@ -3,13 +3,46 @@
 RSpec.describe 'MeiliSearch::Index - Cropped search' do
   before(:all) do
     @documents = [
-      { objectId: 123,  title: 'Pride and Prejudice',                    genre: 'romance' },
-      { objectId: 456,  title: 'Le Petit Prince',                        genre: 'adventure' },
-      { objectId: 1,    title: 'Alice In Wonderland',                    genre: 'adventure' },
-      { objectId: 2,    title: 'Le Rouge et le Noir',                    genre: 'romance' },
-      { objectId: 1344, title: 'The Hobbit',                             genre: 'adventure' },
-      { objectId: 4,    title: 'Harry Potter and the Half-Blood Prince', genre: 'fantasy' },
-      { objectId: 42,   title: 'The Hitchhiker\'s Guide to the Galaxy' }
+      {
+        objectId: 123,
+        title: 'Pride and Prejudice',
+        genre: 'romance',
+        description: 'Pride and Prejudice is a romantic novel of manners written by Jane Austen in 1813.'
+      },
+      {
+        objectId: 456, title: 'Le Petit Prince',
+        genre: 'adventure',
+        description: 'Le Petit Prince is a novella by French aristocrat, writer, and aviator Antoine de Saint-Exupéry.'
+      },
+      {
+        objectId: 1,
+        title: 'Alice In Wonderland',
+        genre: 'adventure',
+        desription: 'Alice\'s Adventures in Wonderland is an 1865 novel by English author Lewis Carroll.'
+      },
+      {
+        objectId: 2,
+        title: 'Le Rouge et le Noir',
+        genre: 'romance',
+        description: 'Le Rouge et le Noir is a historical psychological novel in two volumes by Stendhal.'
+      },
+      {
+        objectId: 1344,
+        title: 'The Hobbit',
+        genre: 'adventure',
+        description: 'The Hobbit is a children\'s fantasy novel by English author J. R. R. Tolkien.'
+      },
+      {
+        objectId: 4,
+        title: 'Harry Potter and the Half-Blood Prince',
+        genre: 'fantasy',
+        description: 'Harry Potter and the Half-Blood Prince is a fantasy novel written by J.K. Rowling.'
+      },
+      {
+        objectId: 42,
+        title: 'The Hitchhiker\'s Guide to the Galaxy',
+        description: 'The Hitchhiker\'s Guide to the Galaxy is a comedy science fiction series by Douglas Adams.'
+      }
     ]
     client = MeiliSearch::Client.new($URL, $MASTER_KEY)
     clear_all_indexes(client)
@@ -35,15 +68,15 @@ RSpec.describe 'MeiliSearch::Index - Cropped search' do
   end
 
   it 'does a custom search with attributes to crop' do
-    response = @index.search('guide', { attributesToCrop: ['title'], cropLength: 2 })
+    response = @index.search('galaxy', { attributesToCrop: ['description'], cropLength: 15 })
     expect(response['hits'].first).to have_key('_formatted')
-    expect(response['hits'].first['_formatted']['title']).to eq('s Guide')
+    expect(response['hits'].first['_formatted']['description']).to eq('s Guide to the Galaxy is a comedy')
   end
 
   it 'does a custom placehodler search with attributes to crop' do
-    response = @index.search('', { attributesToCrop: ['title'], cropLength: 1 })
+    response = @index.search('', { attributesToCrop: ['description'], cropLength: 20 })
     expect(response['hits'].first).to have_key('_formatted')
-    expect(response['hits'].first['title']).to eq(@documents.first[:title])
-    expect(response['hits'].first['_formatted']['title']).not_to eq(@documents.first[:title])
+    expect(response['hits'].first['description']).to eq(@documents.first[:description])
+    expect(response['hits'].first['_formatted']['description']).not_to eq(@documents.first[:description])
   end
 end
