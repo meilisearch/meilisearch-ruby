55df6df1de2229249ccf1d1cdac2ab208b6801e0
CaroFG <carolina.ferreira131@gmail.com>
2020-11-12 18:25:23 +0100

    Add year and author to documents to test a more realistic faceted distribution

--- spec/meilisearch/index/search/facets_distribution_spec.rb
+++ spec/meilisearch/index/search/facets_distribution_spec.rb
@@ -3,18 +3,74 @@
 RSpec.describe 'MeiliSearch::Index - Search with facetsDistribution' do
   before(:all) do
     @documents = [
-      { objectId: 123,  title: 'Pride and Prejudice',                    genre: 'romance' },
-      { objectId: 456,  title: 'Le Petit Prince',                        genre: 'adventure' },
-      { objectId: 1,    title: 'Alice In Wonderland',                    genre: 'adventure' },
-      { objectId: 2,    title: 'Le Rouge et le Noir',                    genre: 'romance' },
-      { objectId: 1344, title: 'The Hobbit',                             genre: 'adventure' },
-      { objectId: 4,    title: 'Harry Potter and the Half-Blood Prince', genre: 'fantasy' },
-      { objectId: 42,   title: 'The Hitchhiker\'s Guide to the Galaxy' }
+      {
+        objectId: 123,
+        title: 'Pride and Prejudice',
+        year: '1813',
+        author: 'Jane Austen',
+        genre: 'romance'
+      },
+      {
+        objectId: 456,
+        title: 'Le Petit Prince',
+        year: '1943',
+        author: 'Antoine de Saint-Exupéry',
+        genre: 'adventure'
+      },
+      {
+        objectId: 1,
+        title: 'Alice In Wonderland',
+        year: '1865',
+        author: 'Lewis Carroll',
+        genre: 'adventure'
+      },
+      {
+        objectId: 2,
+        title: 'Le Rouge et le Noir',
+        year: '1830',
+        author: 'Stendhal',
+        genre: 'romance'
+      },
+      {
+        objectId: 1344,
+        title: 'The Hobbit',
+        year: '1937',
+        author: 'J. R. R. Tolkien',
+        genre: 'adventure'
+      },
+      {
+        objectId: 4,
+        title: 'Harry Potter and the Half-Blood Prince',
+        year: '2005',
+        author: 'J. K. Rowling',
+        genre: 'fantasy'
+      },
+      {
+        objectId: 2056,
+        title: 'Harry Potter and the Deathly Hallows',
+        year: '2007',
+        author: 'J. K. Rowling',
+        genre: 'fantasy'
+      },
+      {
+        objectId: 42,
+        title: 'The Hitchhiker\'s Guide to the Galaxy',
+        year: '1978',
+        author: 'Douglas Adams'
+      },
+      {
+        objectId: 190,
+        title: 'A Game of Thrones',
+        year: '1996',
+        author: 'George R. R. Martin',
+        genre: 'fantasy'
+      }
     ]
     client = MeiliSearch::Client.new($URL, $MASTER_KEY)
     clear_all_indexes(client)
     @index = client.create_index('books')
     @index.add_documents(@documents)
+    @index.update_attributes_for_faceting(['genre', 'year', 'author'])
     sleep(0.1)
   end
 
@@ -35,9 +91,7 @@ RSpec.describe 'MeiliSearch::Index - Search with facetsDistribution' do
   end
 
   it 'does a custom search with facetsDistribution' do
-    @index.update_attributes_for_faceting(['genre'])
-    sleep(0.1)
-    response = @index.search('prinec', facetsDistribution: ['genre'])
+    response = @index.search('prinec', facetsDistribution: ['genre', 'author'])
     expect(response.keys).to contain_exactly(
       *default_search_response_keys,
       'facetsDistribution',
@@ -45,17 +99,17 @@ RSpec.describe 'MeiliSearch::Index - Search with facetsDistribution' do
     )
     expect(response['exhaustiveFacetsCount']).to be true
     expect(response['nbHits']).to eq(2)
-    expect(response['facetsDistribution'].keys).to contain_exactly('genre')
+    expect(response['facetsDistribution'].keys).to contain_exactly('genre', 'author')
     expect(response['facetsDistribution']['genre'].keys).to contain_exactly('romance', 'adventure', 'fantasy')
     expect(response['facetsDistribution']['genre']['romance']).to eq(0)
     expect(response['facetsDistribution']['genre']['adventure']).to eq(1)
     expect(response['facetsDistribution']['genre']['fantasy']).to eq(1)
+    expect(response['facetsDistribution']['author']['J. K. Rowling']).to eq(1)
+    expect(response['facetsDistribution']['author']['Antoine de Saint-Exupéry']).to eq(1)
   end
 
   it 'does a custom placeholder search with facetsDistribution' do
-    @index.update_attributes_for_faceting(['genre'])
-    sleep(0.1)
-    response = @index.search('', facetsDistribution: ['genre'])
+    response = @index.search('', facetsDistribution: ['genre', 'author'])
     expect(response.keys).to contain_exactly(
       *default_search_response_keys,
       'facetsDistribution',
@@ -63,10 +117,11 @@ RSpec.describe 'MeiliSearch::Index - Search with facetsDistribution' do
     )
     expect(response['exhaustiveFacetsCount']).to be true
     expect(response['nbHits']).to eq(@documents.count)
-    expect(response['facetsDistribution'].keys).to contain_exactly('genre')
+    expect(response['facetsDistribution'].keys).to contain_exactly('genre', 'author')
     expect(response['facetsDistribution']['genre'].keys).to contain_exactly('romance', 'adventure', 'fantasy')
     expect(response['facetsDistribution']['genre']['romance']).to eq(2)
     expect(response['facetsDistribution']['genre']['adventure']).to eq(3)
-    expect(response['facetsDistribution']['genre']['fantasy']).to eq(1)
+    expect(response['facetsDistribution']['genre']['fantasy']).to eq(3)
+    expect(response['facetsDistribution']['author']['J. K. Rowling']).to eq(2)
   end
 end
