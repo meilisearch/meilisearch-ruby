5ec5cc7602719e6c98990a1be3594d94978ad31d
CaroFG <carolina.ferreira131@gmail.com>
2020-11-12 17:50:36 +0100

    Add year to documents to test a more realistic faceted search

--- spec/meilisearch/index/search/facet_filters_spec.rb
+++ spec/meilisearch/index/search/facet_filters_spec.rb
@@ -3,19 +3,20 @@
 RSpec.describe 'MeiliSearch::Index - Search with facetFilters' do
   before(:all) do
     @documents = [
-      { objectId: 123,  title: 'Pride and Prejudice',                    genre: 'romance' },
-      { objectId: 456,  title: 'Le Petit Prince',                        genre: 'adventure' },
-      { objectId: 1,    title: 'Alice In Wonderland',                    genre: 'adventure' },
-      { objectId: 2,    title: 'Le Rouge et le Noir',                    genre: 'romance' },
-      { objectId: 1344, title: 'The Hobbit',                             genre: 'adventure' },
-      { objectId: 4,    title: 'Harry Potter and the Half-Blood Prince', genre: 'fantasy' },
-      { objectId: 42,   title: 'The Hitchhiker\'s Guide to the Galaxy' }
+      { objectId: 123,  title: 'Pride and Prejudice',                    year: '1813', genre: 'romance' },
+      { objectId: 456,  title: 'Le Petit Prince',                        year: '1943', genre: 'adventure' },
+      { objectId: 1,    title: 'Alice In Wonderland',                    year: '1865', genre: 'adventure' },
+      { objectId: 2,    title: 'Le Rouge et le Noir',                    year: '1830', genre: 'romance' },
+      { objectId: 1344, title: 'The Hobbit',                             year: '1937', genre: 'adventure' },
+      { objectId: 4,    title: 'Harry Potter and the Half-Blood Prince', year: '2005', genre: 'fantasy' },
+      { objectId: 2056, title: 'Harry Potter and the Deathly Hallows',   year: '2007', genre: 'fantasy' },
+      { objectId: 42,   title: 'The Hitchhiker\'s Guide to the Galaxy',  year: '1978' }
     ]
     client = MeiliSearch::Client.new($URL, $MASTER_KEY)
     clear_all_indexes(client)
     @index = client.create_index('books')
     @index.add_documents(@documents)
-    @index.update_attributes_for_faceting(['genre'])
+    @index.update_attributes_for_faceting(['genre', 'year'])
     sleep(0.1)
   end
 
@@ -43,7 +44,7 @@ RSpec.describe 'MeiliSearch::Index - Search with facetFilters' do
   end
 
   it 'does a custom search with multiple facetFilters' do
-    response = @index.search('prinec', facetFilters: ['genre:fantasy', ['genre:fantasy', 'genre:fantasy']])
+    response = @index.search('potter', facetFilters: ['genre:fantasy', ['year:2005']])
     expect(response.keys).to contain_exactly(*default_search_response_keys)
     expect(response['nbHits']).to eq(1)
     expect(response['hits'][0]['objectId']).to eq(4)
@@ -52,14 +53,15 @@ RSpec.describe 'MeiliSearch::Index - Search with facetFilters' do
   it 'does a custom placeholder search with facetFilters' do
     response = @index.search('', facetFilters: ['genre:fantasy'])
     expect(response.keys).to contain_exactly(*default_search_response_keys)
-    expect(response['nbHits']).to eq(1)
+    expect(response['nbHits']).to eq(2)
     expect(response['hits'][0]['objectId']).to eq(4)
+    expect(response['hits'][1]['objectId']).to eq(2056)
   end
 
   it 'does a custom placeholder search with multiple facetFilters' do
-    response = @index.search('', facetFilters: ['genre:fantasy', ['genre:fantasy', 'genre:fantasy']])
+    response = @index.search('', facetFilters: ['genre:adventure', ['year:1937']])
     expect(response.keys).to contain_exactly(*default_search_response_keys)
     expect(response['nbHits']).to eq(1)
-    expect(response['hits'][0]['objectId']).to eq(4)
+    expect(response['hits'][0]['objectId']).to eq(1344)
   end
 end
