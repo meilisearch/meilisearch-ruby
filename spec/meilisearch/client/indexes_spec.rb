b6a5a917e45293f7bb9d17433b2b0349f99df756
Clementine Urquizar <clementine@meilisearch.com>
2020-11-24 18:35:41 +0100

    Update code and tests with implicit index creation changes

--- spec/meilisearch/client/indexes_spec.rb
+++ spec/meilisearch/client/indexes_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe 'MeiliSearch::Client - Indexes' do
     expect(index).to be_a(MeiliSearch::Index)
     expect(index.uid).to eq(@uid1)
     expect(index.primary_key).to be_nil
+    expect(index.fetch_primary_key).to be_nil
   end
 
   it 'creates an index with primary-key' do
@@ -24,6 +25,7 @@ RSpec.describe 'MeiliSearch::Client - Indexes' do
     expect(index).to be_a(MeiliSearch::Index)
     expect(index.uid).to eq(@uid2)
     expect(index.primary_key).to eq(@primary_key)
+    expect(index.fetch_primary_key).to eq(@primary_key)
   end
 
   it 'creates an index with uid in options - should not take it into account' do
@@ -31,31 +33,37 @@ RSpec.describe 'MeiliSearch::Client - Indexes' do
     expect(index).to be_a(MeiliSearch::Index)
     expect(index.uid).to eq(@uid3)
     expect(index.primary_key).to eq(@primary_key)
+    expect(index.fetch_primary_key).to eq(@primary_key)
   end
 
   it 'creates an new index with get_or_create_index method' do
     index = @client.get_or_create_index(@uid4)
+    expect(index).to be_a(MeiliSearch::Index)
     expect(@client.indexes.count).to eq(4)
-    expect(@client.index(@uid4).uid).to eq(index.uid)
-    expect(@client.index(@uid4).uid).to eq(@uid4)
-    expect(@client.index(@uid4).primary_key).to be_nil
+    expect(@client.fetch_index(@uid4).uid).to eq(index.uid)
+    expect(@client.fetch_index(@uid4).uid).to eq(@uid4)
+    expect(@client.fetch_index(@uid4).primary_key).to be_nil
+    expect(@client.fetch_index(@uid4).primary_key).to eq(index.primary_key)
   end
 
   it 'creates an new index with get_or_create_index method and a primary-key' do
     index = @client.get_or_create_index(@uid5, primaryKey: 'title')
+    expect(index).to be_a(MeiliSearch::Index)
     expect(@client.indexes.count).to eq(5)
-    expect(@client.index(@uid5).uid).to eq(index.uid)
-    expect(@client.index(@uid5).uid).to eq(@uid5)
-    expect(@client.index(@uid5).primary_key).to eq(index.primary_key)
-    expect(@client.index(@uid5).primary_key).to eq('title')
+    expect(@client.fetch_index(@uid5).uid).to eq(index.uid)
+    expect(@client.fetch_index(@uid5).uid).to eq(@uid5)
+    expect(@client.fetch_index(@uid5).primary_key).to eq(index.primary_key)
+    expect(@client.fetch_index(@uid5).primary_key).to eq('title')
   end
 
   it 'get an already existing index with get_or_create_index method' do
     index = @client.get_or_create_index(@uid5)
+    expect(index).to be_a(MeiliSearch::Index)
     expect(@client.indexes.count).to eq(5)
-    expect(@client.index(@uid5).uid).to eq(index.uid)
-    expect(@client.index(@uid5).uid).to eq(@uid5)
-    expect(@client.index(@uid5).primary_key).to eq('title')
+    expect(@client.fetch_index(@uid5).uid).to eq(index.uid)
+    expect(@client.fetch_index(@uid5).uid).to eq(@uid5)
+    expect(@client.fetch_index(@uid5).primary_key).to eq('title')
+    expect(@client.fetch_index(@uid5).primary_key).to eq(index.primary_key)
   end
 
   it 'fails to create an index with an uid already taken' do
@@ -78,35 +86,34 @@ RSpec.describe 'MeiliSearch::Client - Indexes' do
     expect(uids).to contain_exactly(@uid1, @uid2, @uid3, @uid4, @uid5)
   end
 
-  it 'shows a specific index' do
-    response = @client.show_index(@uid2)
-    expect(response).to be_a(Hash)
-    expect(response['uid']).to eq(@uid2)
-    expect(response['primaryKey']).to eq(@primary_key)
+  it 'fetch a specific index' do
+    response = @client.fetch_index(@uid2)
+    expect(response).to be_a(MeiliSearch::Index)
+    expect(response.uid).to eq(@uid2)
+    expect(response.primary_key).to eq(@primary_key)
+    expect(response.fetch_primary_key).to eq(@primary_key)
   end
 
   it 'returns an index object based on uid' do
     index = @client.index(@uid2)
     expect(index).to be_a(MeiliSearch::Index)
     expect(index.uid).to eq(@uid2)
+    expect(index.primary_key).to be_nil
+    expect(index.fetch_primary_key).to eq(@primary_key)
     expect(index.primary_key).to eq(@primary_key)
   end
 
   it 'deletes index' do
     expect(@client.delete_index(@uid1)).to be_nil
-    expect { @client.show_index(@uid1) }.to raise_index_not_found_meilisearch_api_error
+    expect { @client.fetch_index(@uid1) }.to raise_index_not_found_meilisearch_api_error
     expect(@client.delete_index(@uid2)).to be_nil
-    expect { @client.show_index(@uid2) }.to raise_index_not_found_meilisearch_api_error
+    expect { @client.fetch_index(@uid2) }.to raise_index_not_found_meilisearch_api_error
     expect(@client.delete_index(@uid3)).to be_nil
-    expect { @client.show_index(@uid3) }.to raise_index_not_found_meilisearch_api_error
+    expect { @client.fetch_index(@uid3) }.to raise_index_not_found_meilisearch_api_error
     expect(@client.delete_index(@uid4)).to be_nil
-    expect { @client.show_index(@uid4) }.to raise_index_not_found_meilisearch_api_error
+    expect { @client.fetch_index(@uid4) }.to raise_index_not_found_meilisearch_api_error
     expect(@client.delete_index(@uid5)).to be_nil
-    expect { @client.show_index(@uid5) }.to raise_index_not_found_meilisearch_api_error
+    expect { @client.fetch_index(@uid5) }.to raise_index_not_found_meilisearch_api_error
     expect(@client.indexes.count).to eq(0)
-  end
-
-  it 'works with method aliases' do
-    expect(@client.method(:index) == @client.method(:get_index)).to be_truthy
   end
 end
--- spec/meilisearch/index/base_spec.rb
+++ spec/meilisearch/index/base_spec.rb
@@ -11,21 +11,21 @@ RSpec.describe MeiliSearch::Index do
     @index2 = client.create_index(@uid2, primaryKey: @primary_key)
   end
 
-  it 'shows the index' do
-    response = @index1.show
-    expect(response).to be_a(Hash)
-    expect(response['name']).to eq(@uid1)
-    expect(response['uid']).to eq(@uid1)
-    expect(response['uid']).to eq(@index1.uid)
-    expect(response['primaryKey']).to be_nil
+  it 'fetch the info of the index' do
+    index = @index1.fetch_info
+    expect(index).to be_a(MeiliSearch::Index)
+    expect(index.uid).to eq(@uid1)
+    expect(index.primary_key).to be_nil
   end
 
   it 'get primary-key of index if null' do
     expect(@index1.primary_key).to be_nil
+    expect(@index1.fetch_primary_key).to be_nil
   end
 
   it 'get primary-key of index if it exists' do
     expect(@index2.primary_key).to eq(@primary_key)
+    expect(@index2.fetch_primary_key).to eq(@primary_key)
   end
 
   it 'get uid of index' do
@@ -34,10 +34,11 @@ RSpec.describe MeiliSearch::Index do
 
   it 'updates primary-key of index if not defined before' do
     new_primary_key = 'id_test'
-    response = @index1.update(primaryKey: new_primary_key)
-    expect(response).to be_a(Hash)
-    expect(response['uid']).to eq(@uid1)
-    expect(@index1.primary_key).to eq(new_primary_key)
+    index = @index1.update(primaryKey: new_primary_key)
+    expect(index).to be_a(MeiliSearch::Index)
+    expect(index.uid).to eq(@uid1)
+    expect(index.primary_key).to eq(new_primary_key)
+    expect(index.fetch_primary_key).to eq(new_primary_key)
   end
 
   it 'returns error if trying to update primary-key if it is already defined' do
@@ -53,21 +54,20 @@ RSpec.describe MeiliSearch::Index do
 
   it 'deletes index' do
     expect(@index1.delete).to be_nil
-    expect { @index1.show }.to raise_index_not_found_meilisearch_api_error
+    expect { @index1.fetch_info }.to raise_index_not_found_meilisearch_api_error
     expect(@index2.delete).to be_nil
-    expect { @index2.show }.to raise_index_not_found_meilisearch_api_error
+    expect { @index2.fetch_info }.to raise_index_not_found_meilisearch_api_error
   end
 
   it 'fails to manipulate index object after deletion' do
-    expect { @index2.primary_key }.to raise_index_not_found_meilisearch_api_error
-    expect { @index2.show }.to raise_index_not_found_meilisearch_api_error
+    expect { @index2.fetch_primary_key }.to raise_index_not_found_meilisearch_api_error
+    expect { @index2.fetch_info }.to raise_index_not_found_meilisearch_api_error
     expect { @index2.update(primaryKey: 'id_test') }.to raise_index_not_found_meilisearch_api_error
     expect { @index2.delete }.to raise_index_not_found_meilisearch_api_error
   end
 
   it 'works with method aliases' do
-    expect(@index1.method(:show) == @index1.method(:show_index)).to be_truthy
-    expect(@index1.method(:primary_key) == @index1.method(:get_primary_key)).to be_truthy
+    expect(@index1.method(:fetch_primary_key) == @index1.method(:get_primary_key)).to be_truthy
     expect(@index1.method(:update) == @index1.method(:update_index)).to be_truthy
     expect(@index1.method(:delete) == @index1.method(:delete_index)).to be_truthy
   end
