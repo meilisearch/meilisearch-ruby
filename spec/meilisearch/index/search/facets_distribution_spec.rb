d6d52d74d4919cda62a70fdb38afa6e5fa2114df
Jason Schweier <jasons@nulogy.com>
2021-10-04 18:39:16 -0400

    Use shared context 'search books with author, genre, year'

--- spec/meilisearch/index/search/facets_distribution_spec.rb
+++ spec/meilisearch/index/search/facets_distribution_spec.rb
@@ -1,78 +1,9 @@
 # frozen_string_literal: true
 
 RSpec.describe 'MeiliSearch::Index - Search with facetsDistribution' do
-  let(:index) { test_client.create_index('books') }
-  let(:documents) do
-    [
-      {
-        objectId: 123,
-        title: 'Pride and Prejudice',
-        year: 1813,
-        author: 'Jane Austen',
-        genre: 'romance'
-      },
-      {
-        objectId: 456,
-        title: 'Le Petit Prince',
-        year: 1943,
-        author: 'Antoine de Saint-Exupéry',
-        genre: 'adventure'
-      },
-      {
-        objectId: 1,
-        title: 'Alice In Wonderland',
-        year: 1865,
-        author: 'Lewis Carroll',
-        genre: 'adventure'
-      },
-      {
-        objectId: 2,
-        title: 'Le Rouge et le Noir',
-        year: 1830,
-        author: 'Stendhal',
-        genre: 'romance'
-      },
-      {
-        objectId: 1344,
-        title: 'The Hobbit',
-        year: 1937,
-        author: 'J. R. R. Tolkien',
-        genre: 'adventure'
-      },
-      {
-        objectId: 4,
-        title: 'Harry Potter and the Half-Blood Prince',
-        year: 2005,
-        author: 'J. K. Rowling',
-        genre: 'fantasy'
-      },
-      {
-        objectId: 2056,
-        title: 'Harry Potter and the Deathly Hallows',
-        year: 2007,
-        author: 'J. K. Rowling',
-        genre: 'fantasy'
-      },
-      {
-        objectId: 42,
-        title: 'The Hitchhiker\'s Guide to the Galaxy',
-        year: 1978,
-        author: 'Douglas Adams'
-      },
-      {
-        objectId: 190,
-        title: 'A Game of Thrones',
-        year: 1996,
-        author: 'George R. R. Martin',
-        genre: 'fantasy'
-      }
-    ]
-  end
+  include_context 'search books with author, genre, year'
 
   before do
-    response = index.add_documents(documents)
-    index.wait_for_pending_update(response['updateId'])
-
     response = index.update_filterable_attributes(['genre', 'year', 'author'])
     index.wait_for_pending_update(response['updateId'])
   end
