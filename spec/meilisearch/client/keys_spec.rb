143f5e731e3b4e31cee58ad8b47dc47681770977
Jason Schweier <jasons@nulogy.com>
2021-10-04 09:20:14 -0400

    Update client keys specs to be independent

--- spec/meilisearch/client/keys_spec.rb
+++ spec/meilisearch/client/keys_spec.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe 'MeiliSearch::Client - Keys' do
-  before(:all) do
-    @client = MeiliSearch::Client.new(URL, MASTER_KEY)
-    @uid = 'uid'
-    @client.create_index(@uid)
-  end
-
-  after(:all) { clear_all_indexes(@client) }
-
   it 'gets the list of keys' do
-    response = @client.keys
+    response = test_client.keys
     expect(response).to be_a(Hash)
     expect(response.count).to eq(2)
     expect(response.keys).to contain_exactly('private', 'public')
@@ -19,15 +11,15 @@ RSpec.describe 'MeiliSearch::Client - Keys' do
   end
 
   it 'fails to get settings if public key used' do
-    public_key = @client.keys['public']
+    public_key = test_client.keys['public']
     new_client = MeiliSearch::Client.new(URL, public_key)
     expect do
-      new_client.index(@uid).settings
+      new_client.index(test_uid).settings
     end.to raise_meilisearch_api_error_with(403, 'invalid_token', 'authentication_error')
   end
 
   it 'fails to get keys if private key used' do
-    private_key = @client.keys['private']
+    private_key = test_client.keys['private']
     new_client = MeiliSearch::Client.new(URL, private_key)
     expect do
       new_client.keys
@@ -37,25 +29,28 @@ RSpec.describe 'MeiliSearch::Client - Keys' do
   it 'fails to search if no key used' do
     new_client = MeiliSearch::Client.new(URL)
     expect do
-      new_client.index(@uid).settings
+      new_client.index(test_uid).settings
     end.to raise_meilisearch_api_error_with(401, 'missing_authorization_header', 'authentication_error')
   end
 
   it 'succeeds to search when using public key' do
-    public_key = @client.keys['public']
+    public_key = test_client.keys['public']
+    response = test_index.add_documents(title: 'Test')
+    test_index.wait_for_pending_update(response['updateId'])
+
     new_client = MeiliSearch::Client.new(URL, public_key)
-    response = new_client.index(@uid).search('test')
+    response = new_client.index(test_uid).search('test')
     expect(response).to have_key('hits')
   end
 
   it 'succeeds to get settings when using private key' do
-    private_key = @client.keys['private']
+    private_key = test_client.keys['private']
     new_client = MeiliSearch::Client.new(URL, private_key)
-    response = new_client.index(@uid).settings
+    response = new_client.index(test_uid).settings
     expect(response).to have_key('rankingRules')
   end
 
   it 'works with method aliases' do
-    expect(@client.method(:keys) == @client.method(:get_keys)).to be_truthy
+    expect(test_client.method(:keys) == test_client.method(:get_keys)).to be_truthy
   end
 end
