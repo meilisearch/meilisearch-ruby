2cd0fcf2b83d4e263ac764543379a854e16e267f
Jason Schweier <jasons@nulogy.com>
2021-10-20 10:07:51 -0400

    Start SimpleCov before loading application to track coverage for lib/ code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,11 +16,14 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
+# NOTE: If SimpleCov starts after your application code is already loaded (via require),
+# it won't be able to track your files and their coverage!
+# The SimpleCov.start must be issued before any of your application code is required!
+require 'simplecov'
+SimpleCov.start
+
 require 'meilisearch'
 require 'byebug'
-require 'simplecov'
-
-SimpleCov.start
 
 # Globals for all tests
 URL = 'http://localhost:7700'
