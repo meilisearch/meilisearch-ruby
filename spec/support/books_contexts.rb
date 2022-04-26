3afa2c7900665122d5a4220c9a990a21676645af
Bruno Casali <brunoocasali@gmail.com>
2022-04-26 14:43:09 -0300

    Add nested fields support (#318)

--- spec/support/books_contexts.rb
+++ spec/support/books_contexts.rb
@@ -94,3 +94,68 @@ RSpec.shared_context 'search books with author, genre, year' do
     index.wait_for_task(response['uid'])
   end
 end
+
+RSpec.shared_context 'search books with nested fields' do
+  let(:index) { client.index('books') }
+  let(:documents) do
+    [
+      {
+        id: 1,
+        title: 'Pride and Prejudice',
+        info: {
+          comment: 'A great book',
+          reviewNb: 50
+        }
+      },
+      {
+        id: 2,
+        title: 'Le Petit Prince',
+        info: {
+          comment: 'A french book',
+          reviewNb: 600
+        }
+      },
+      {
+        id: 3,
+        title: 'Le Rouge et le Noir',
+        info: {
+          comment: 'Another french book',
+          reviewNb: 700
+        }
+      },
+      {
+        id: 4,
+        title: 'Alice In Wonderland',
+        info: {
+          comment: 'A weird book',
+          reviewNb: 800
+        }
+      },
+      {
+        id: 5,
+        title: 'The Hobbit',
+        info: {
+          comment: 'An awesome book',
+          reviewNb: 900
+        }
+      },
+      {
+        id: 6,
+        title: 'Harry Potter and the Half-Blood Prince',
+        info: {
+          comment: 'The best book',
+          reviewNb: 1000
+        }
+      },
+      {
+        id: 7,
+        title: 'The Hitchhiker\'s Guide to the Galaxy'
+      }
+    ]
+  end
+
+  before do
+    response = index.add_documents(documents)
+    index.wait_for_task(response['uid'])
+  end
+end
