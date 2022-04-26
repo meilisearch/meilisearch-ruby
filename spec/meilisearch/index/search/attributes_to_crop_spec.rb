b6f9ea49ff48f3f5e326fd8d5e9df75a6f49c7c1
Bruno Casali <brunoocasali@gmail.com>
2022-04-26 14:17:14 -0300

    Fix tests according to the v0.27 (#317)
    * Fix tests according to the v0.27
    
    * Re-run rubocop --auto-gen-config

--- spec/meilisearch/index/search/attributes_to_crop_spec.rb
+++ spec/meilisearch/index/search/attributes_to_crop_spec.rb
@@ -13,15 +13,15 @@ RSpec.describe 'MeiliSearch::Index - Cropped search' do
   before { index.add_documents!(document) }
 
   it 'does a custom search with attributes to crop' do
-    response = index.search('galaxy', { attributesToCrop: ['description'], cropLength: 15 })
+    response = index.search('galaxy', { attributesToCrop: ['description'], cropLength: 6 })
     expect(response['hits'].first).to have_key('_formatted')
-    expect(response['hits'].first['_formatted']['description']).to eq('s Guide to the Galaxy is a comedy science')
+    expect(response['hits'].first['_formatted']['description']).to eq('…Guide to the Galaxy is a…')
   end
 
   it 'does a placehodler search with attributes to crop' do
-    response = index.search('', { attributesToCrop: ['description'], cropLength: 20 })
+    response = index.search('', { attributesToCrop: ['description'], cropLength: 5 })
     expect(response['hits'].first).to have_key('_formatted')
     expect(response['hits'].first['description']).to eq(document[:description])
-    expect(response['hits'].first['_formatted']['description']).to eq("The Hitchhiker\'s Guide")
+    expect(response['hits'].first['_formatted']['description']).to eq("The Hitchhiker\'s Guide to…")
   end
 end
