d9452161f721ebf2bb0343018b223a6d1673c613
Clémentine Urquizar <clementine@meilisearch.com>
2022-06-14 17:16:02 +0200

    Update showMatchesPosition with show_matches_position

--- spec/meilisearch/index/search/matches_spec.rb
+++ spec/meilisearch/index/search/matches_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'MeiliSearch::Index - Search with showMatchesPosition' do
   include_context 'search books with genre'
 
   it 'does a custom search with showMatchesPosition' do
-    response = index.search('the', showMatchesPosition: true)
+    response = index.search('the', show_matches_position: true)
     expect(response).to be_a(Hash)
     expect(response.keys).to contain_exactly(*DEFAULT_SEARCH_RESPONSE_KEYS)
     expect(response['hits'].first).to have_key('_matchesPosition')
@@ -12,7 +12,7 @@ RSpec.describe 'MeiliSearch::Index - Search with showMatchesPosition' do
   end
 
   it 'does a placeholder search with showMatchesPosition' do
-    response = index.search('', showMatchesPosition: true)
+    response = index.search('', show_matches_position: true)
     expect(response).to be_a(Hash)
     expect(response.keys).to contain_exactly(*DEFAULT_SEARCH_RESPONSE_KEYS)
     expect(response['hits'].first).to have_key('_matchesPosition')
