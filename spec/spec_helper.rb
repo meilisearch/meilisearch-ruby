d0104a369e2652ee437c2bc857bea3b1753d1cf7
Dibash Thapa <dibashthapa55@gmail.com>
2022-10-11 22:36:03 +0545

    chores: replace MEILISEARCH_HOST with MEILISEARCH_URL

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,7 +29,7 @@ require 'byebug'
 
 # Globals for all tests
 URL = format('http://%<host>s:%<port>s',
-             host: ENV.fetch('MEILISEARCH_HOST', 'localhost'), port: ENV.fetch('MEILISEARCH_PORT', '7700'))
+             host: ENV.fetch('MEILISEARCH_URL', 'localhost'), port: ENV.fetch('MEILISEARCH_PORT', '7700'))
 MASTER_KEY = 'masterKey'
 DEFAULT_SEARCH_RESPONSE_KEYS = [
   'hits',
