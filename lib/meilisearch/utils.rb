f7c39c762b432ff250b50afd465d9112b6a3c4df
Bruno Casali <brunoocasali@gmail.com>
2022-10-21 14:54:44 -0300

    Rename MeiliSearch to Meilisearch in the deprecation warning message

--- lib/meilisearch/utils.rb
+++ lib/meilisearch/utils.rb
@@ -29,7 +29,7 @@ module MeiliSearch
       return if non_snake_case.empty?
 
       message = <<~MSG
-        Attributes will be expected to be snake_case in future versions of MeiliSearch.
+        Attributes will be expected to be snake_case in future versions of Meilisearch Ruby.
 
         Non-conforming attributes: #{non_snake_case.join(', ')}
       MSG
