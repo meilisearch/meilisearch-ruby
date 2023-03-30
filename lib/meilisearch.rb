83953f671f80a3d4e06e33cf6cfd3f8409029934
Bruno Casali <brunoocasali@gmail.com>
2023-03-30 14:48:26 +0100

    Create MultiSearch module

--- lib/meilisearch.rb
+++ lib/meilisearch.rb
@@ -3,6 +3,7 @@
 require 'meilisearch/version'
 require 'meilisearch/utils'
 require 'meilisearch/http_request'
+require 'meilisearch/multi_search'
 require 'meilisearch/tenant_token'
 require 'meilisearch/task'
 require 'meilisearch/client'
---
+++ spec/meilisearch/client/multi_search_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe 'MeiliSearch::Client - Multiple Index Search' do
+  before do
+    client.create_index('books')
+    client.create_index('movies')
+  end
+
+  it 'does a custom search with two different indexes' do
+    response = client.multi_search([
+                                     { index_uid: 'books', q: 'prince' },
+                                     { index_uid: 'movies', q: 'prince' }
+                                   ])
+
+    expect(response['results'].count).to eq(2)
+    expect(response['results'][0]['estimatedTotalHits']).to eq(0)
+    expect(response['results'][1]['estimatedTotalHits']).to eq(0)
+  end
+end
