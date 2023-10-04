2529dea4cb82e3977b4d84da7a5e5476857b5a4e
ellnix <103502144+ellnix@users.noreply.github.com>
2023-10-04 18:20:41 +0200

    Add tests for new Error structure

--- spec/meilisearch/client/errors_spec.rb
+++ spec/meilisearch/client/errors_spec.rb
@@ -1,6 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe 'MeiliSearch::Client - Errors' do
+  describe 'MeiliSearch::Error' do
+    it 'catches MeiliSearch::TimeoutError' do
+      expect do
+        raise MeiliSearch::TimeoutError
+      end.to raise_error(MeiliSearch::Error)
+    end
+
+    it 'catches MeiliSearch::CommunicationError' do
+      expect do
+        raise MeiliSearch::CommunicationError, ''
+      end.to raise_error(MeiliSearch::Error)
+    end
+
+    it 'catches MeiliSearch::ApiError' do
+      expect do
+        raise MeiliSearch::ApiError.new(200, '', '')
+      end.to raise_error(MeiliSearch::Error)
+    end
+  end
+
   context 'when request takes to long to answer' do
     it 'raises MeiliSearch::TimeoutError' do
       timed_client = MeiliSearch::Client.new(URL, MASTER_KEY, { timeout: 0.000001 })
