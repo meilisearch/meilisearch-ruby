e753f456ea35550588c31c5e2b505c1071e37a8e
Andre <>
2024-07-06 18:37:28 +0200

    Tests fixed

--- spec/meilisearch/index/search/ranking_score_threshold_spec.rb
+++ spec/meilisearch/index/search/ranking_score_threshold_spec.rb
@@ -4,10 +4,10 @@ RSpec.describe 'MeiliSearch::Index - Search with rankingScoreThreshold' do
   include_context 'search books with genre'
 
   it 'does a custom search with rankingScoreThreshold' do
-    response = index.search('harry potter and the chamber of secrets', { rankingScoreThreshold: 0.9 })
+    response = index.search('harry potter and the prisoner of azkaban', { rankingScoreThreshold: 0.9 })
     expect(response['hits'].count).to be(0)
 
     response = index.search('harry potter and the', { rankingScoreThreshold: 0.3 })
-    expect(response['hits'].count).to be(1)
+    expect(response['hits'].count).to be(2)
   end
 end
