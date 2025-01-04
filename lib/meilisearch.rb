a25439fed2175ba62470df84af8a9badc33ece8c
ellnix <ellnix@disroot.org>
2025-01-04 23:42:34 +0100

    Add deprecation message for top-level module

--- lib/meilisearch.rb
+++ lib/meilisearch.rb
@@ -14,3 +14,45 @@ require 'meilisearch/index'
 
 module Meilisearch
 end
+
+module MeiliSearch
+  # Softly deprecate the old spelling of the top level module
+  # from MeiliSearch to Meilisearch
+  def self.const_missing(const_name)
+    return super if @warned
+
+    Meilisearch::Utils.logger.warn <<~RENAMED_MODULE_WARNING
+      [meilisearch-ruby] The top-level module of Meilisearch has been renamed.
+      [meilisearch-ruby] Please update "MeiliSearch" to "Meilisearch".
+    RENAMED_MODULE_WARNING
+
+    Meilisearch.constants.each do |constant|
+      const_set(constant, Meilisearch.const_get(constant))
+    end
+
+    @warned = true
+
+    # Now that all the proper constants have been set,
+    # we can tell ruby to search for the const in MeiliSearch again.
+    # If it's still not found, then it does not exist in
+    # Meilisearch and the call to `super` will throw a normal error
+    const_get(const_name)
+  end
+
+  def self.method_missing(method_name)
+    unless @warned
+      Meilisearch::Utils.logger.warn <<~RENAMED_MODULE_WARNING
+        [meilisearch-ruby] The top-level module of Meilisearch has been renamed.
+        [meilisearch-ruby] Please update "MeiliSearch" to "Meilisearch".
+      RENAMED_MODULE_WARNING
+
+      Meilisearch.constants.each do |constant|
+        const_set(constant, Meilisearch.const_get(constant))
+      end
+    end
+
+    @warned = true
+
+    Meilisearch.send(method_name)
+  end
+end
