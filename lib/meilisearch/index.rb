54aa0f5e7ebad1bc4fceb59e6fff89483df977bf
Evgeniy Fateev <spaceflow@gmail.com>
2025-12-12 11:52:16 +0500

    Add index compaction

--- lib/meilisearch/index.rb
+++ lib/meilisearch/index.rb
@@ -698,6 +698,19 @@ module Meilisearch
       stats['isIndexing']
     end
 
+    ### COMPACT
+
+    # Run database compaction for this index.
+    #
+    # @note Meilisearch must temporarily duplicate the database during compaction. You need at least twice the current size of your database in free disk space.
+    #
+    # @see https://www.meilisearch.com/docs/reference/api/compact Meilisearch API Reference
+    # @return [Models::Task] The index compaction async task.
+    def compact
+      response = http_post "/indexes/#{@uid}/compact"
+      Models::Task.new(response, task_endpoint)
+    end
+
     # Get the filed distribution of documents in the index.
     #
     # Calls {#stats}
